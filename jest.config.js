400a6888f0c349149a187a5f44646b40b97e7648
devbran <d-evbran@outlook.com>
2023-08-28 13:42:53 -0300

    feat(navigation): ok

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  preset: 'react-native',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
