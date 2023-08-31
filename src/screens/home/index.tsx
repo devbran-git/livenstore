fc6d26dd320ccac3d6588b88c9365e8c85dbc9d8
devbran <d-evbran@outlook.com>
2023-08-31 08:03:55 -0300

    feat: category selection implemented

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,9 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {
   View,
   Text,
   FlatList,
+  ScrollView,
   SafeAreaView,
   TouchableOpacity,
 } from 'react-native';
@@ -22,6 +23,7 @@ import {CartProduct} from 'hooks/cart/cart.types';
 
 import styles from './styles';
 import ProductModal from 'components/productModal/productModal';
+import {Category} from './types';
 
 const Home: React.FC = () => {
   const {navigate} = useNavigation<NavigationProps>();
@@ -31,21 +33,47 @@ const Home: React.FC = () => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<Product>();
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const [productsToDisplay, setProductsToDisplay] = useState<Product[]>();
+
+  const categories = [
+    {categoryId: 'all', name: 'All categories'},
+    {categoryId: 'women', name: 'Women'},
+    {categoryId: 'men', name: 'Men'},
+    {categoryId: 'jewelery', name: 'Jewelery'},
+    {categoryId: 'electronics', name: 'Electronics'},
+  ];
+
+  const allProducts = useMemo(() => products, [products]);
+
+  const womenCategory = products.filter(product =>
+    product.category.startsWith('women'),
+  );
+
+  const menCategory = products.filter(product =>
+    product.category.startsWith('men'),
+  );
+
+  const jeweleryCategory = products.filter(
+    product => product.category === 'jewelery',
+  );
+
+  const electronicsCategory = products.filter(
+    product => product.category === 'electronics',
+  );
 
   const handleNavigateToCart = () => navigate('Cart');
 
   const handleCloseModal = () => setIsModalOpen(false);
 
   const handleOpenModal = (productId: number) => {
-    const clickedProduct = productsToDisplay.find(
+    const clickedProduct = productsToDisplay?.find(
       product => product?.id === productId,
     );
 
     setSelectedProduct(clickedProduct);
     setIsModalOpen(true);
   };
-
-  const productsToDisplay = useMemo(() => products, [products]);
 
   const handleAddProductsToCart = (product: Product, productCount: number) => {
     const productIsInCart = cart.find(
@@ -59,6 +87,24 @@ const Home: React.FC = () => {
 
     if (!productIsInCart) addProductToCart(newProductToCart);
   };
+
+  const handleSelectCategory = (type: string) => {
+    setSelectedCategory(type);
+
+    const options: Category = {
+      all: () => setProductsToDisplay(allProducts),
+      women: () => setProductsToDisplay(womenCategory),
+      men: () => setProductsToDisplay(menCategory),
+      jewelery: () => setProductsToDisplay(jeweleryCategory),
+      electronics: () => setProductsToDisplay(electronicsCategory),
+    };
+
+    options[type]();
+  };
+
+  useEffect(() => {
+    setProductsToDisplay(allProducts);
+  }, []);
 
   return (
     <>
@@ -77,26 +123,51 @@ const Home: React.FC = () => {
         {isLoading ? (
           <Skeleton />
         ) : (
-          <View style={styles.productListContainer}>
-            <FlatList
-              data={productsToDisplay}
-              contentContainerStyle={styles.productList}
-              keyExtractor={item => String(item?.id)}
-              renderItem={({item, index}) => (
-                <ProductCard
-                  index={index}
-                  product={item}
-                  handleAddProductsToCart={handleAddProductsToCart}
-                  handleOpenModal={handleOpenModal}
-                />
-              )}
-              numColumns={2}
-              ItemSeparatorComponent={() => (
-                <View style={styles.listSeparator} />
-              )}
-              showsVerticalScrollIndicator={false}
-            />
-          </View>
+          <>
+            <View style={styles.productListContainer}>
+              <View style={styles.categoryListContainer}>
+                <ScrollView
+                  contentContainerStyle={styles.categoryList}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}>
+                  {categories.map(category => (
+                    <TouchableOpacity
+                      key={String(category.categoryId)}
+                      activeOpacity={0.7}
+                      onPress={() => handleSelectCategory(category.categoryId)}>
+                      <Text
+                        style={[
+                          styles.categoryLabel,
+                          selectedCategory === category.categoryId &&
+                            styles.boldCategory,
+                        ]}>
+                        {category.name}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
+              </View>
+
+              <FlatList
+                data={productsToDisplay}
+                contentContainerStyle={styles.productList}
+                keyExtractor={item => String(item?.id)}
+                renderItem={({item, index}) => (
+                  <ProductCard
+                    index={index}
+                    product={item}
+                    handleAddProductsToCart={handleAddProductsToCart}
+                    handleOpenModal={handleOpenModal}
+                  />
+                )}
+                numColumns={2}
+                ItemSeparatorComponent={() => (
+                  <View style={styles.listSeparator} />
+                )}
+                showsVerticalScrollIndicator={false}
+              />
+            </View>
+          </>
         )}
       </SafeAreaView>
 
